f921052b179dbc2bff5714f1f6766355834140c2
mllemango <msmangochan@gmail.com>
2019-06-26 15:49:44 -0400

    finished formualting, now to debug

--- a2.py
+++ a2.py
@@ -13,14 +13,16 @@ def info():
     # warehouses
     WHs = []
     WH_cap = []
-    WH_cost = []
+    WH_Cost = []
     with open('WH.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             WHs.append(row[0])
             WH_cap.append(row[1])
-            WH_cost.append(row[2])
-
+            WH_Cost.append(row[2])
+    # print(WHs)
+    # print(WH_cap)
+    # print(WH_Cost)
     # customers
     custs = []
     cust_demand = []
@@ -53,7 +55,7 @@ def info():
             dist = line[2]
             WH_cust_distance[(cust, WH)] = dist
 
-    # print(WH_cust_distance[('Greater Sudbury', 'London')])
+    print(WH_cust_distance[('Greater Sudbury', 'London')])
     return plants, WHs, custs, plant_cap, plant_cost, WH_cap, WH_Cost, cust_demand, plant_WH_distance, WH_cust_distance
 
 
@@ -64,27 +66,47 @@ def optimize(plants, WHs, custs, plant_cap, plant_cost, WH_cap, WH_Cost, cust_de
     P = len(plants)
     W = len(WHs)
     C = len(custs)
-    tij = m.addVars(P, W, vtype=GRB.INTEGER)  # number of goods from plant i to WH j
-    Wj = m.addVars(W, vtype=GRB.BINARY)  # 1 if warehouse j operates
-    Cjk = m.addVars(W, C, vtype=GRB.INTEGER)  # number of goods from WH j to cust k
+    tij = m.addVars(plants, WHs, vtype=GRB.INTEGER)  # number of goods from plant i to WH j
+    Wj = m.addVars(WHs, vtype=GRB.BINARY)  # 1 if warehouse j operates
+    Cjk = m.addVars(WHs, custs, vtype=GRB.INTEGER)  # number of goods from WH j to cust k
 
     # constants
     dij = plant_WH_distance
     djk = WH_cust_distance
-    Cj = WH_cost
+    Cj = WH_Cost
     Ci = plant_cost
     Wpj = WH_cap
+    Cdk = cust_demand
+    Pci = plant_cap  # [1500, 1500]
+
+    # print(dij)
 
     # objective function
     m.setObjective(quicksum(quicksum(tij[i, j] * dij[i, j] for i in range(P)) for j in range(W)) * COST_KM +
                    quicksum(Wj[j] * Cj[j] for j in range(W)) +
-                   quicksum(quicsum(Cjk * djk for j in range(W)) for k in range(C)) +
-                   quicksum(Ci for i in range(P)), GRB.MINIMIZE)
+                   quicksum(quicksum(Cjk[j, k] * djk[j, k] for j in range(W)) for k in range(C)) +
+                   quicksum(Ci[i] for i in range(P)), GRB.MINIMIZE)  # last summation is a constant
 
     # constraints
+    # Plant constraints
+    for i in range(P):
+        m.addConstr(quicksum(tij[i, j] for j in range(W) <= Pci[i]))  # goods going out of plant i does not go over plant capacity
+    # WH constraints --> 1 constraint for each WH
     for j in range(W):
-        m.addConstr(quicksum(tij for i in range(P)) <= Wpj[j])
-        m.addContr()
+        # m.addConstr(quicksum(tij[i, j] for i in range(P)) <= Wpj[j])  # WH goods do not exceed capcity
+        m.addConstr(quicksum(tij[i, j] for i in range(P)) == quicksum(Cjk[j, k] for k in range(C)))  # goods going into WH = goods going out of WH
+    # Customer demand
+    for k in range(C):
+        m.addConstr(quicksum(Cjk[j, k] for j in range(W)) == Cdk[k])  # demand is met for Cust K
+    # Operations
+    for j in range(W):
+        m.addConstr(Wj[j] * Wpj[j] - quicksum(tij[i, j] for i in range(P)) >= 0)  # if WH j is not operating, nothing goes out of it
+
+    m.update()
+    m.optimize()
+
+    # printing output
+    print('Min Cost', m.objVal)
 
 
 if __name__ == "__main__":
